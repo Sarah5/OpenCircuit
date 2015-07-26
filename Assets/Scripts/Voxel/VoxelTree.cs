a3934f1e9626d29c68a806d42fe8c72968ff4c38
semimono <culleneason@gmail.com>
2015-07-26 01:45:09 -0600

    Fixed some voxel bugs (A lot).

--- Assets/Scripts/Voxel/VoxelTree.cs
+++ Assets/Scripts/Voxel/VoxelTree.cs
@@ -70,9 +70,6 @@ namespace Vox {
 				s /= 2;
 			}
 			updateCheckJobs = 0;
-			lock(this) {
-				cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-			}
 
 			// initialize voxels
 			head = new VoxelBlock();
@@ -142,6 +139,9 @@ namespace Vox {
 		}
 
 		public void updateLocalCamPosition() {
+			lock(this) {
+				cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+			}
 			localCamPosition = transform.TransformPoint(cam.transform.position);
 		}
 
@@ -240,6 +240,18 @@ namespace Vox {
 					e.Current.clear();
 				}
 			}
+			List<Transform> children = new List<Transform>(transform.childCount);
+			foreach(Transform child in transform) {
+				if ((child.hideFlags &  HideFlags.DontSave) != 0)
+					children.Add(child);
+			}
+			foreach(Transform child in children) {
+				GameObject.DestroyImmediate(child.gameObject);
+			}
+			foreach(MeshCollider collider in GetComponents<MeshCollider>()) {
+				if ((collider.hideFlags & HideFlags.HideInInspector) != 0)
+					GameObject.DestroyImmediate(collider);
+			}
 		}
 
 		public void generateRenderers() {
@@ -249,9 +261,6 @@ namespace Vox {
 		}
 
 		public void wipe() {
-//			lock(this) {
-//				jobQueue.Clear();
-//			}
 			clearRenderers();
 			if (head != null) {
 				head = null;
@@ -279,19 +288,13 @@ namespace Vox {
 		}
 
 		public void OnAfterDeserialize() {
+			clearRenderers();
 			if (voxelData.Length > 0) {
 				MemoryStream stream = new MemoryStream(voxelData);
 				BinaryReader reader = new BinaryReader(stream);
 				head = (VoxelBlock)VoxelHolder.deserialize(reader);
 				stream.Close();
 			}
-//			lock(renderers) {
-//				foreach (VoxelRenderer rend in renderers.Values) {
-//					rend.control = this;
-////					rend.setupMeshes();
-//					((VoxelBlock)(getHead().get(rend.index))).renderer = rend;
-//				}
-//			}
 		}
 		
 		public bool import(string fileName) {
