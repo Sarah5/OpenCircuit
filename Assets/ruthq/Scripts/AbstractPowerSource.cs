fbcc0122af702a5539d00583e6e1df974237f81e
Ruthq <charispoch@gmail.com>
2015-08-08 16:42:38 -0600

    Initial work on robot power system

---
+++ Assets/ruthq/Scripts/AbstractPowerSource.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbstractPowerSource : MonoBehaviour {
+	public abstract bool drawPower (float amount);
+
+}
---
+++ Assets/ruthq/Scripts/PowerGenorator.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerGenerator : AbstractPowerSource {
+	public override bool drawPower (float amount){
+		return true; 
+	}
+}
