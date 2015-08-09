0bebc65947fa8e53e2cc6b966bac3bfaf28643f4
Ruthq <charispoch@gmail.com>
2015-08-08 18:29:51 -0600

    Subtract power from battery when drawn

--- Assets/ruthq/Scripts/Battery.cs
+++ Assets/ruthq/Scripts/Battery.cs
@@ -2,7 +2,24 @@
 using System.Collections;
 
 public class Battery : AbstractPowerSource {
-	public override bool drawPower (float amount){
-		return true; 
+
+	public float maximumCapacity = 100;
+	public float currentCapacity = 100;
+
+	void Update () {
+		drawPower (0.01f);
 	}
+
+	public override bool drawPower (float amount){
+		if (currentCapacity < amount){
+			return false;
+		}
+		float subtractAmount = amount - currentCapacity;
+			return true;
+
+
+	
+	}
+
+
 }
