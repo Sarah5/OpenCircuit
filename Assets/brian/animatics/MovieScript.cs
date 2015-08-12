9f64d79385396d8b83736d7789968151ac54f1fa
michbr <bmichaud45@gmail.com>
2015-08-11 19:34:22 -0600

    Reorganization of the script folders - moved prototype scripts into the final location

---
+++ Assets/brian/animatics/MovieScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+	}
+}
