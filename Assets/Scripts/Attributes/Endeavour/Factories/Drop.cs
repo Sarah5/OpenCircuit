241117d636a67bf9e4f1ca4fc264773723d97934
michbr <bmichaud45@gmail.com>
2015-08-21 10:08:35 -0600

    Initial work on EMP

---
+++ Assets/Scripts/Attributes/Endeavour/Factories/Drop.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Drop : EndeavourFactory {
+
+	public override Endeavour constructEndeavour (RobotController controller) {
+		if (parent == null) {
+			return null;
+		}
+		return new DropKickAction(controller, parent);
+	}
+	
+	public override void doGUI() {
+	}
+}
---
+++ Assets/Scripts/Attributes/Equipables/EMP.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EMP : Equipable {
+	protected override Trigger buildTrigger() {
+		return null;
+	}
+
+	protected override Trigger getTrigger() {
+		return null;
+	}
+
+	public override void invoke(GameObject target){
+		print ("invoked");
+	}
+
+
+}
