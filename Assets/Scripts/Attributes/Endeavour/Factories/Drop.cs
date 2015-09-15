4798f85aec85f61792e44f394f4855211ef1c8f4
michbr <bmichaud45@gmail.com>
2015-09-15 12:49:12 -0600

    Generalize the drop gizmo code.

--- Assets/Scripts/Attributes/Endeavour/Factories/Drop.cs
+++ Assets/Scripts/Attributes/Endeavour/Factories/Drop.cs
@@ -4,6 +4,13 @@ using System.Collections;
 [System.Serializable]
 public class Drop : EndeavourFactory {
 
+	public const float NUM_STRIPES = 8f;
+	public const float LENGTH = 1f;
+
+
+	public static Color COLOR_ONE = Color.black;
+	public static Color COLOR_TWO = Color.yellow;
+
 	public override Endeavour constructEndeavour (RobotController controller) {
 		if (parent == null) {
 			return null;
@@ -19,6 +26,12 @@ public class Drop : EndeavourFactory {
 		Gizmos.color = Color.red;
 		Gizmos.DrawSphere(parent.transform.position, .1f);
 		Gizmos.color = Color.gray;
-		Gizmos.DrawRay(new Ray(parent.transform.position + (parent.transform.forward * .1f), parent.transform.forward));
+		//Gizmos.DrawRay(new Ray(parent.transform.position + (parent.transform.forward * .1f), parent.transform.forward));
+		for (int i = 0; i < NUM_STRIPES; i ++) {
+			Gizmos.color = i % 2 == 0 ? COLOR_ONE : COLOR_TWO;
+			Vector3 startPos = parent.transform.position + (parent.transform.forward * .1f) + ((i * (LENGTH/NUM_STRIPES))*parent.transform.forward);
+			Vector3 endPos = startPos + (((LENGTH/NUM_STRIPES))*parent.transform.forward);
+			Gizmos.DrawLine(startPos, endPos);
+		}
 	}
 }
