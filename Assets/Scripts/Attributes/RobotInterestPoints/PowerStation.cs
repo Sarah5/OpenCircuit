88414c4392d71dff7ce7d39d0605e74d572dd2f9
Ruthq <charispoch@gmail.com>
2015-10-04 20:21:58 -0600

    Created power generator to recharge robot batteries.

---
+++ Assets/Scripts/Attributes/RobotInterestPoints/PowerStation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerStation : MonoBehaviour
+{
+
+    void OnTriggerStay(Collider collision) {
+        Battery battery = collision.gameObject.GetComponent<Battery>();
+        if (battery != null) {
+            battery.addPower(20f * Time.deltaTime);
+        }
+    }
+}
--- Assets/ruthq/Scripts/Battery.cs
+++ Assets/ruthq/Scripts/Battery.cs
@@ -7,18 +7,18 @@ public class Battery : AbstractPowerSource {
 	public float maximumCapacity = 100;
 	public float currentCapacity = 100;
 
-	void Update () {
-		drawPower (1.0f * Time.deltaTime);
-	}
-
 	public override bool drawPower (float amount){
 		if (currentCapacity < amount){
 			return false;
 		}
-		float subtractAmount = currentCapacity - amount;
-		currentCapacity = subtractAmount;
+	   currentCapacity = currentCapacity - amount;
 		return base.drawPower(amount) && true;
 	}
+    public void addPower (float amount) {
+        if (currentCapacity < maximumCapacity) {
+            currentCapacity = currentCapacity + amount;
+        }
+    }
 
 
 }
