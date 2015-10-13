1adf6cf4fb58e5c0a60641f05945894e9a5aa4df
michbr <bmichaud45@gmail.com>
2015-10-13 01:08:22 -0600

    Initial work on Robot hearing

---
+++ Assets/Scripts/Attributes/AudioEvent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioEvent {
+
+    private Vector3 source;
+    private Tag descriptor;
+
+    public AudioEvent(Vector3 source, Tag descriptor) {
+        this.source = source;
+        this.descriptor = descriptor;
+    }
+
+    public Tag getTag() {
+        return descriptor;
+    }
+
+    public Vector3 getSourcePosition() {
+        return source;
+    }
+
+}
