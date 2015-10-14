10d506077c434fabfc506a52147c3081d30a8094
Ruthq <charispoch@gmail.com>
2015-10-13 22:56:49 -0600

    Add IdleAction and RechargeAction.

---
+++ Assets/Scripts/Attributes/Endeavour/IdleAction.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class IdleAction : Endeavour {
+
+    public IdleAction(RobotController controller, List<Goal> goals, Label target) : base(controller, goals, target.gameObject) {
+
+    }
+
+    public override void execute() {
+
+     }
+
+    public override void stopExecution() {
+
+    }
+
+    public override void onMessage(RobotMessage message) {
+       
+    }
+
+    public override bool isStale() {
+      return false;
+    }
+
+    protected override float getCost() {
+        return 0f;
+    }
+}
---
+++ Assets/Scripts/Attributes/Endeavour/RechargeAction.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class RechargeAction : Endeavour {
+
+    private Label powerStation;
+
+    public RechargeAction(RobotController controller, List<Goal> goals, Label target) : base(controller, goals, target.gameObject) {
+        powerStation = target;
+        this.name = "recharge";
+        requiredComponents = new System.Type[] { typeof(HoverJet) };
+    }
+
+    public override void execute() {
+        HoverJet jet = controller.GetComponentInChildren<HoverJet>();
+        if (jet != null) {
+            jet.setTarget(powerStation);
+            jet.setAvailability(false);
+        }
+    }
+
+    public override bool isStale() {
+        return powerStation == null;
+    }
+
+    public override void onMessage(RobotMessage message) {
+      
+    }
+
+    public override void stopExecution() {
+        HoverJet jet = controller.GetComponentInChildren<HoverJet>();
+        if (jet != null) {
+            jet.setTarget(null);
+            jet.setAvailability(true);
+        }
+    }
+
+    protected override float getCost() {
+        return 0f;
+    }
+}
