cde4581341c33ad34f0ace68d18a67ed75b7d5a0
semimono <culleneason@gmail.com>
2015-10-19 10:10:16 -0600

    Added throwable rocks.  Still need refinement.

---
+++ Assets/Scripts/Items/Rock.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[AddComponentMenu("Scripts/Items/Rock")]
+public class Rock : MonoBehaviour {
+
+	public static int maxRocks = 10;
+
+	public float volume = 0.1f;
+	public float volumeThreshold = 0.1f;
+	public AudioClip clip;
+
+	protected AudioSource source;
+
+	private static List<Rock> rocks = new List<Rock>(maxRocks);
+
+	public void Start () {
+		rocks.Add(this);
+		while(rocks.Count > maxRocks) {
+			GameObject.Destroy(rocks[0].gameObject);
+			rocks.RemoveAt(0);
+		}
+
+		source = GetComponent<AudioSource>();
+		if (source == null)
+			source = gameObject.AddComponent<AudioSource>();
+	}
+
+	public void OnDestroy() {
+		rocks.Remove(this);
+	}
+
+	public void OnCollisionEnter(Collision col) {
+		float volume = col.impulse.magnitude *this.volume;
+		if (volume > volumeThreshold)
+			source.PlayOneShot(clip, volume);
+	}
+}
---
+++ Assets/Scripts/Items/Rocks.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Scripts/Items/Rocks")]
+public class Rocks : Item {
+
+	public Vector3 throwVelocity = new Vector3(0, 2f, 15);
+	public Vector3 releaseLocation = new Vector3(0.4f, 0.3f, 0.3f);
+	public Mesh rockMesh;
+	public Material rockMaterial;
+	public AudioClip collisionSound;
+	
+
+	public override void invoke(Inventory invoker) {
+		Transform cam = invoker.getPlayer().cam.transform;
+        GameObject rock = new GameObject("Rock");
+		rock.transform.localPosition = cam.TransformPoint(releaseLocation);
+		rock.transform.localScale *= 0.2f;
+		rock.AddComponent<MeshFilter>().sharedMesh = rockMesh;
+		rock.AddComponent<MeshRenderer>().sharedMaterial = rockMaterial;
+		SphereCollider col = rock.AddComponent<SphereCollider>();
+		//col.radius
+		//col.convex = true;
+		Rigidbody rb = rock.AddComponent<Rigidbody>();
+		rb.velocity = cam.TransformVector(throwVelocity);
+		rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+		rock.AddComponent<Rock>().clip = collisionSound;
+	}
+
+	public override void onEquip(Inventory equipper) {
+		base.onEquip(equipper);
+	}
+
+	public override void onUnequip(Inventory equipper) {
+		base.onUnequip(equipper);
+	}
+}
