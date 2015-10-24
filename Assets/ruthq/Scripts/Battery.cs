7e4eb1a7e0b2822d94f0cd5c08ca6e99fb24b6bb
Ruthq <charispoch@gmail.com>
2015-10-24 12:32:24 -0600

    Added hasPower method.

--- Assets/ruthq/Scripts/Battery.cs
+++ Assets/ruthq/Scripts/Battery.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 [AddComponentMenu("Scripts/Robot/Battery")]
 public class Battery : AbstractPowerSource {
@@ -7,18 +8,23 @@ public class Battery : AbstractPowerSource {
 	public float maximumCapacity = 100;
 	public float currentCapacity = 100;
 
-	public override bool hasPower(float amount){
+	public override bool drawPower(float amount){
 		if (currentCapacity < amount){
 			return false;
 		}
 	   currentCapacity = currentCapacity - amount;
 		return base.drawPower(amount) && true;
 	}
-    public void addPower (float amount) {
+    public void addPower (float amount) { 
         if (currentCapacity < maximumCapacity) {
             currentCapacity = currentCapacity + amount;
         }
     }
 
-
+    public override bool hasPower(float amount) {
+        if (currentCapacity < amount) {
+            return false;
+        }
+        return true;
+    }
 }
