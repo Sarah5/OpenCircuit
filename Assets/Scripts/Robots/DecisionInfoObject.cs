027137cf110e2ced7d5780019542c9c5b9b7456c
michbr <bmichaud45@gmail.com>
2015-10-27 00:01:45 -0600

    Add an object necessary for the robot debug gui

---
+++ Assets/Scripts/Robots/DecisionInfoObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DecisionInfoObject {
+	private string name;
+	private float priority;
+	private bool chosen;
+
+
+	public DecisionInfoObject(string name, float priority, bool chosen) {
+		this.name = name;
+		this.priority = priority;
+		this.chosen = chosen;
+	}
+
+	public string getTitle() {
+		return name;
+	}
+
+	public float getPriority() {
+		return priority;
+	}
+
+	public bool isChosen() {
+		return chosen;
+	}
+}
+
